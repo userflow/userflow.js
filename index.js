7e9bf2ae991171125c1a1652f9c2f8792937a457
Sebastian Seilund <sebsei@gmail.com>
2019-11-11 07:07:18 -0800

    Use regular functions instead of arrow functions in order to not require transpilation of userflow.js to run in IE11. Note that a Promise polyfill is still required.

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ let loadUserflowPromise
 module.exports.loadUserflow = function(opts) {
   opts = opts || {}
   if (!loadUserflowPromise) {
-    loadUserflowPromise = new Promise((resolve, reject) => {
+    loadUserflowPromise = new Promise(function(resolve, reject) {
       const script = document.createElement('script')
       script.src = opts.url || 'https://js.getuserflow.com/userflow.js'
-      script.onload = () => {
+      script.onload = function() {
         if (!window.userflow) {
           reject(
             new Error(
@@ -17,7 +17,7 @@ module.exports.loadUserflow = function(opts) {
         }
         resolve(window.userflow)
       }
-      script.onerror = () => {
+      script.onerror = function() {
         loadUserflowPromise = undefined
         reject(new Error('Could not load Userflow.js'))
       }
