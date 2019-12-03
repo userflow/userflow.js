2d7b9d31f931cab83ad49bccbeab2250d8160a8c
Sebastian Seilund <sebsei@gmail.com>
2019-12-02 16:54:55 -0800

    Added typings for new inference config options

--- index.d.ts
+++ index.d.ts
@@ -1,19 +1,40 @@
 export interface Userflow {
   init: (token: string) => void
+
   identify: (externalId: string, params: IdentifyParams) => Promise<void>
+
   updateUser: (params: IdentifyParams) => Promise<void>
+
   isIdentified: () => boolean
+
   startFlow: (flowId: string) => Promise<void>
+
   endAllFlows: () => Promise<void>
+
   reset: () => void
+
   on(eventName: string, listener: (...args: any[]) => void): void
+
   off(eventName: string, listener: (...args: any[]) => void): void
+
   setCustomInputSelector(customInputSelector: string | null): void
+
   registerCustomInput(
     cssSelector: string,
     getValue?: (el: Element) => string
   ): void
+
   setCustomNavigate(customNavigate: ((url: string) => void) | null): void
+
+  setInferenceAttributeNames(attributeNames: string[]): void
+
+  setInferenceAttributeFilter(
+    attributeName: string,
+    filters: StringFilters
+  ): void
+
+  setInferenceClassNameFilter(filters: StringFilters): void
+
   setScrollPadding(scrollPadding: ScrollPadding | null): void
 }
 
@@ -50,11 +71,15 @@ interface LoadUserflowOpts {
   url?: string
 }
 
-export interface ScrollPadding {
+interface ScrollPadding {
   top?: number
   right?: number
   bottom?: number
   left?: number
 }
+
+type StringFilter = ((className: string) => boolean) | RegExp
+
+type StringFilters = StringFilter | StringFilter[]
 
 export function loadUserflow(opts?: LoadUserflowOpts): Promise<Userflow>
